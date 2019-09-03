e4faa5e6f45281ef54342c3a02032a86343896e4
heriyanto <heriyanto.liu@sidomuncul.co.id>
2019-09-03 09:42:32 +0700

    exercise 6.9

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AnecdotesForm from './components/AnecdotesForm';
 import AnecdoteList from './components/AnecdoteList';
+import Notification from './components/Notification';
 
 const App = props => {
 	return (
 		<div>
+			<Notification store={props.store} />
 			<h2>Anecdotes</h2>
 			<AnecdoteList store={props.store} />
 			<AnecdotesForm store={props.store} />
---
+++ anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,19 @@
+const initialMessage = 'Test Notification';
+
+const notificationReducer = (state = initialMessage, action) => {
+	switch (action.type) {
+		case 'SET_NOTIFICATION':
+			return action.message;
+		default:
+			return state;
+	}
+};
+
+export const setNotification = message => {
+	return {
+		type: 'SET_NOTIFICATION',
+		message
+	};
+};
+
+export default notificationReducer;
